a964e802b2c8e524cc4c639599bb5b3dbe941846
ramasandeep-2003 <124776899+ramasandeep-2003@users.noreply.github.com>
2023-02-16 15:20:05 +0530

    Add files via upload

---
+++ StringTrimExample.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class StringTrimExample {
+    public static void main(String args[]){
+        String s1="  hello string   ";
+        System.out.println(s1+"javatpoint");
+        System.out.println(s1.trim()+"javatpoint");
+    }
+}
